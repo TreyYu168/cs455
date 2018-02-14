e00d7b45b187138978fad48156faa4b9369f4d32
Trey Yu <trey@Treys-MacBook-Pro.local>
2018-02-13 21:48:52 -0700

    Implemented more wireformats

--- src/overlay/wireformats/OverlayNodeSendsData.java
+++ src/overlay/wireformats/OverlayNodeSendsData.java
@@ -1,4 +1,57 @@
 package overlay.wireformats;
 
-public class OverlayNodeSendsData {
+import java.io.*;
+
+public class OverlayNodeSendsData implements Event{
+
+    final static byte messageType = 9;
+    private int destinationID;
+    private int sourceID;
+    private int payload;
+    private int numOfHops;
+    private int[] traveledNodes;
+
+    public OverlayNodeSendsData(byte[] marshaledBytes) throws IOException {
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(marshaledBytes);
+        DataInputStream dataInputStream = new DataInputStream(byteArrayInputStream);
+
+        byte type = dataInputStream.readByte();
+        this.destinationID = dataInputStream.readInt();
+        this.sourceID = dataInputStream.readInt();
+        this.payload = dataInputStream.readInt();
+        this.numOfHops = dataInputStream.readInt();
+
+        this.traveledNodes = new int[numOfHops];
+        for (int i = 0; i < numOfHops; i++) {
+            traveledNodes[i] = dataInputStream.readInt();
+        }
+
+        byteArrayInputStream.close();
+        dataInputStream.close();
+
+    }
+
+    @Override
+    public byte[] getBytes() throws IOException {
+        byte[] marshaledBytes;
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        DataOutputStream dataOutputStream = new DataOutputStream(byteArrayOutputStream);
+
+        dataOutputStream.writeByte(messageType);
+        dataOutputStream.writeInt(destinationID);
+        dataOutputStream.writeInt(sourceID);
+        dataOutputStream.writeInt(payload);
+        dataOutputStream.writeInt(numOfHops);
+        for(int i = 0; i < numOfHops; i++) {
+            dataOutputStream.writeInt(traveledNodes[i]);
+        }
+
+        dataOutputStream.flush();
+        marshaledBytes = byteArrayOutputStream.toByteArray();
+
+        return marshaledBytes;
+    }
+
+    @Override
+    public byte getType() { return this.messageType; }
 }
--- src/overlay/wireformats/RegistryRequestsTaskInitiate.java
+++ src/overlay/wireformats/RegistryRequestsTaskInitiate.java
@@ -1,4 +1,42 @@
 package overlay.wireformats;
 
-public class RegistryRequestsTaskInitiate {
+import java.io.*;
+
+public class RegistryRequestsTaskInitiate implements Event{
+
+    final static byte messageType = 8;
+    private int numOfPackets;
+
+    public RegistryRequestsTaskInitiate(byte[] marshaledBytes) throws IOException {
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(marshaledBytes);
+        DataInputStream dataInputStream = new DataInputStream(byteArrayInputStream);
+
+        byte type = dataInputStream.readByte();
+        this.numOfPackets = dataInputStream.readInt();
+
+        byteArrayInputStream.close();
+        dataInputStream.close();
+
+    }
+
+    @Override
+    public byte[] getBytes() throws IOException {
+        byte[] marshaledBytes;
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        DataOutputStream dataOutputStream = new DataOutputStream(byteArrayOutputStream);
+
+        dataOutputStream.writeByte(messageType);
+        dataOutputStream.writeInt(numOfPackets);
+
+        dataOutputStream.flush();
+        marshaledBytes = byteArrayOutputStream.toByteArray();
+
+        dataOutputStream.close();
+        byteArrayOutputStream.close();
+        return marshaledBytes;
+    }
+
+    @Override
+    public byte getType() { return this.messageType; }
+
 }
